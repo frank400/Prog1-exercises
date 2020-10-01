7f2997abb9f8c3680e6f781b7d95b6cede1c7185
Lucas Parente <lucasparente1808@gmail.com>
2020-10-01 20:29:06 -0300

    trocando int pra float

--- first_list/exercise1.c
+++ first_list/exercise1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-float get_expression_result(int x){
+float get_expression_result(float x){
 	//Returns always one, since both expression are equals, but a leave both just to ilustrate.
 	return (x *((-x) + (x * x) - (x * x * x) + (x * x * x * x))) / (-x *((x) - (x * x) + (x * x * x) - (x * x * x * x))) ;
 }
 
 int main(int argc, char const *argv[])
 {
-	int x;
-	scanf("%d",&x);
+	float x;
+	scanf("%f",&x);
 	printf("%f",get_expression_result(x));
 	return 0;
 }
