f190d33c4881efa8443246c4086e8e5593fbf1d6
frank400 <j.victorhuguenin2018@gmail.com>
2020-10-01 08:16:18 -0300

    feat: adding solution to the second problem

---
+++ first_list/exercise2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float add_percentage(float x, int percentage){
+    return x*(1+(percentage/100.0));
+}
+float retrive_percentage(float x,int percentage){
+    return x*(1-(percentage/100.0));
+}
+int main(int argc, char const *argv[])
+{
+    float r1,r2;
+    printf("First real value to be incresed by 30 percent:");
+    scanf("%f",&r1);
+    printf("First real value to be decresed by 25 percent:");
+    scanf("%f",&r2);
+    float r1_increased = add_percentage(r1,30);
+    float r2_decresed = retrive_percentage(r2,25);
+    printf("the final value of the first value is :%f\nthe final value of the second one is:%f\n",r1_increased,r2_decresed);
+    return 0;
+}
