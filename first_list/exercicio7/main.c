dfc7edcc5033d3132298def98f00b697e2f6a947
frank400 <j.victorhuguenin2018@gmail.com>
2020-10-01 20:57:41 -0300

    add solution to problem 7

--- first_list/exercicio7/main.c
+++ first_list/exercicio7/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float get_raiz(float a, float b){
+    return (-b)/a;
+}
 int main()
 {
-    int a,b;
-    scanf("%d %d",&a,&b);
-    a != 0 ;
+    float a,b;
+    scanf("%f %f",&a,&b);
+    printf("A raiz do da função afim é (%.2f,0)\n",get_raiz(a,b));
 
     return 0;
 }
